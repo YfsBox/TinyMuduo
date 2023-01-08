44e704fbc0638b16ffc0843d004eb8d2931c538c
YfsBox <1027342142@qq.com>
2023-01-08 13:15:37 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/TinyMuduo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TinyMuduo.dir/library.cpp.o"
+  "libTinyMuduo.a"
+  "libTinyMuduo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TinyMuduo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
